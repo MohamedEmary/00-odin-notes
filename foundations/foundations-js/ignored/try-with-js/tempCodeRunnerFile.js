9f5efaf80e036a568a2c62d6a17ebe05bcca2b73
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2024-03-13 17:50:25 +0200

    remove files

--- foundations/foundations-js/ignored/try-with-js/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-let myString = "1,2,3,EE,l,z,e,r,o,_,W,e,b,_,S,c,h,o,o,l,2,0,Z";
-let solution = myString.split("").filter(function(ele){
-    return (ele >= 'a' && ele <= 'y') || (ele >= 'A' && ele <= 'Y') || ele === "_";
-}).reduce(function(acc, current){
-    return `${acc}${current}`;
-}).slice(true).replaceAll("_", " ");
-
-console.log(solution);
